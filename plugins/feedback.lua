12e40283205f60f3ea7e763480dfe92704399322
DeterGent-Legion <shayanmayeli@yahoo.com>
2016-02-03 23:15:49 +0330

    Create feedback.lua

---
+++ plugins/feedback.lua
@@ -0,0 +1,27 @@
+do
+
+ function run(msg, matches)
+ local ch = 'chat#id'..msg.to.id
+ local fuse = 'New FeedBack\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username ..'\n\nMessage From: '..msg.to.id.. '\n\nThe Pm:\n' .. matches[1]
+ local fuses = '!printf user#id' .. msg.from.id
+
+
+   local text = matches[1]
+   local chat = "chat#id"..53167206
+
+  local sends = send_msg(chat, fuse, ok_cb, false)
+  return 'Sent!'
+
+ end
+ end
+ return {
+
+  description = "Feedback",
+
+  usage = "!feedback message",
+  patterns = {
+  "^[Ff]eedback (.*)$"
+
+  },
+  run = run
+ }
