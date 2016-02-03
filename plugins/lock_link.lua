d11ccfa51c27c6bbf68f865cab6779217111c061
DeterGent-Legion <shayanmayeli@yahoo.com>
2016-02-03 23:09:02 +0330

    Create lock_link.lua

---
+++ plugins/lock_link.lua
@@ -0,0 +1,30 @@
+
+local function run(msg, matches)
+if msg.to.type == 'chat' then
+    if is_owner(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_link'] then
+                lock_ads = data[tostring(msg.to.id)]['settings']['lock_link']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_ads == "yes" then
+        send_large_msg(chat, 'Ads is not allowed here!!')
+        chat_del_user(chat, user, ok_cb, true)
+    end
+end
+ end
+return {
+  patterns = {
+    "telegram.me/joinchat/",
+        "telegram.me",
+    "[Ht]ttps://"
+  },
+  run = run
+}
