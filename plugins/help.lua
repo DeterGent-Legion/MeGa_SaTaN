532326567c406d90269eb7abcecd40da76c3150d
DeterGent-Legion <shayanmayeli@yahoo.com>
2016-02-05 15:10:26 +0330

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -159,10 +159,10 @@ end
 return {
   description = "Help plugin. Get info from other plugins.  ",
   usage = {
-    "!help: Show list of plugins.",
-    "!help all: Show all commands for every plugin.",
-    "!help [plugin name]: Commands for that plugin.",
-    "!help [number]: Commands for that plugin. Type !help to get the plugin number."
+    "help: Show list of plugins.",
+    "help all: Show all commands for every plugin.",
+    "help [plugin name]: Commands for that plugin.",
+    "help [number]: Commands for that plugin. Type !help to get the plugin number."
   },
   patterns = {
     "^help$",
