c0bac664f911bf44a25cb78530ed27c8abd4ed0f
DeterGent-Legion <shayanmayeli@yahoo.com>
2016-02-06 16:10:29 +0330

    Create copyright.lua

---
+++ plugins/copyright.lua
@@ -0,0 +1,14 @@
+do
+local function run(msg, matches)
+return "....." -- Enter Your Bots About
+end
+return {
+usage ={
+"...: Return Abouts.", -- Enter Yout Bot Name
+},
+pattenrs = {
+"^([])$", -- Enter Your Patterns
+},
+run = run
+}
+end
