8a88711b72284e799e5dc948abc6379af7e428ca
DeterGent-Legion <shayanmayeli@yahoo.com>
2016-02-08 21:40:33 +0330

    Update helper.lua

--- plugins/helper.lua
+++ plugins/helper.lua
@@ -1,14 +1,18 @@
 do
 local function run(msg, matches)
+  if matches[1] == 'chat_add_user' or matches[1] == 'chat_add_user_link' or matches[1] == 'chat_del_user' then
+    return ""
+  end
 return " Use Without Any Symbols."
 end
+
 return {
 usage = {
 "For Help To Begginer Onwers.",
 },
 patterns ={
-"^!",
-"^/",
+"!",
+"!!tgservice (.+)$",
 },
 run = run
 }
