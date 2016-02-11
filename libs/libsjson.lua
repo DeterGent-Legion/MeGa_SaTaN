ff61f1ebb0fd5464892b451e11c38f2ab86a35b7
DeterGent-Legion <shayanmayeli@yahoo.com>
2016-02-11 02:20:08 -0800

    Create libsjson.lua

---
+++ libs/libsjson.lua
@@ -0,0 +1,24 @@
+do local _ = {
+  about_text = "",
+  disabled_channels = {},
+  disabled_plugin_on_chat = {
+
+  },
+  enabled_plugins = {
+
+  },
+  help_text = "",
+  moderation = {
+    
+  },
+  realm = {
+    
+  },
+  sudo_users = {
+    198828890,
+	175531746
+	
+  }
+}
+return _
+end
